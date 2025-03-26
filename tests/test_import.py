4f948ae11b7101048976150808a82b50e429bd99
mguthriem <guthriem@ornl.gov>
2025-03-26 10:49:29 -0400

    failing test

---
+++ tests/test_import.py
@@ -0,0 +1,14 @@
+import pytest
+
+def test_import():
+    import snapblue.blueUtils
+    import snapblue.blueIO
+    import snapblue.SNAPStateMgr
+
+def test_import_fail():
+    with pytest.raises(ImportError):
+        import snapblue.garbage as foo
+
+
+def test_willFail():
+    assert False
