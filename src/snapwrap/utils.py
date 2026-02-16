216626e04a2602a9c46d2629048ebc78e85b2480
mguthriem <guthriem@ornl.gov>
2026-02-16 16:44:45 -0500

    revisited fix for qsp=True issue where pixelgroup information was missing

--- src/snapwrap/utils.py
+++ src/snapwrap/utils.py
@@ -1381,12 +1381,6 @@ def restoreDBins(redObj,originalIngredients):
     #extract ragged binning params from originalIngredients
 
     pgName = redObj.pixelGroup.lower()
-    # print(f"Debug: restoring dbinning for pixel group: {pgName}")
-    # print("DEBUG: originalIngredients contains the following pixel groups: ")
-    print([pg.focusGroup.name for pg in originalIngredients])
-    print("here is the complete original ingredients:")
-    for pg in originalIngredients:
-        print(pg)
 
     dMins = []
     dMaxs = []
@@ -1922,28 +1916,22 @@ def reduce(runNumber,
             hooks = hooks,
         )
 
-
-    # # manually add focus groups to the request.
-    # reductionRequest.focusGroups = groupings["focusGroups"]
-
-    # # print("groupings are: ")
-    # # print(groupings)
-    # # print("should be a valid list [type=list_type, input_value={'focusGroups': [FocusGro...rouping__Column_64413']}, input_type=dict]")
-    # # assert False
+    # manually add focus groups to the request.
+    groupings = reductionService.fetchReductionGroupings(reductionRequest)
+    pgs = groupings["focusGroups"]
+    reductionRequest.focusGroups = pgs
 
     # # print("Debug 1866, pixel group allow list = ",reductionRequest.focusGroupAllowList)
 
     snapRequest = SNAPRequest(path="/reduction",payload=reductionRequest,hooks=hooks)
     reductionService.validateReduction(reductionRequest)
+    # manually add focus groups to the request.
+    groupings = reductionService.fetchReductionGroupings(reductionRequest)
+    pgs = groupings["focusGroups"]
+    reductionRequest.focusGroups = pgs
 
     print("snapRequest:")
     print(snapRequest)
-
-
-
-
-    
-
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # "fetchReductionGroceries" Loads necessary data (e.g. sample neutron data,
@@ -1970,6 +1958,7 @@ def reduce(runNumber,
 
     ingredients = reductionService.prepReductionIngredients(reductionRequest)
     ingredients.artificialNormalizationIngredients = artificialNormalizationIngredients
+
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # Determine calibration status and process this
@@ -2060,15 +2049,14 @@ def reduce(runNumber,
         originalIngredients,ingredients = updateBinForQ(ingredients,linBin)
 
         pgs = ingredients.pixelGroups
-        print("UPDATED")
+
         for pg in pgs:
-            print(f"pgs: {pg.focusGroup.name} with {len(pg.pixelGroupingParameters)} subgroups")
+
             for subgroup in pg.pixelGroupingParameters:
                 params = pg.pixelGroupingParameters[subgroup]
                 dMin = params.dResolution.minimum
                 dMax = params.dResolution.maximum
                 dBin = params.dRelativeResolution/pg.nBinsAcrossPeakWidth
-                print(f"{dMin:.4f} {dBin:.6f} {dMax:.4f}")
 
     if reduceData:
 
